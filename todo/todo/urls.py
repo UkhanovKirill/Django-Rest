bb9fb34e9a53af11481e754f784b499b385148bb
kirill <ukhanovkirill1@gmail.com>
2022-10-06 18:48:25 +0300

    add serializers

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from todo.users.views import UserModelViewSet
 
+router = DefaultRouter()
+router.register('users', UserModelViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls))
 ]
