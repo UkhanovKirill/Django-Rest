f4685945019aaac233524a70912dc3f575f494f3
kirill <ukhanovkirill1@gmail.com>
2022-10-18 14:54:48 +0300

    correction

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from todo.users.views import UserModelViewSet
+from users.views import UserModelViewSet
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
