32add680df5afb8aabe63ad7da691e7c9801d910
kirill <ukhanovkirill1@gmail.com>
2022-10-18 18:41:34 +0300

    add output of users

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import UserList from "./components/User";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'users': []
+    }
+  }
+
+  componentDidMount() {
+
+    axios.get('http://127.0.0.1:8000/api/users/').then(response => {
+      this.setState({
+        'users': response.data
+      }).catch(error => console.log(error))
+    })
+  }
+
+  render() {
+    return (
+        <div>
+          <UserList users={this.state.users}/>
+        </div>
+    )
+  }
+
 }
 
 export default App;
